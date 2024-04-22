002551c6717992fd4949245198f5cbf0235b940b
Ams Rahul <dapurmilky@gmail.com>
2024-04-22 10:46:56 +0800

    test

--- src/app.js
+++ src/app.js
@@ -1,33 +1,36 @@
-import express from 'express';
-import http from 'http';
-import { Server } from 'socket.io';
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
 
-const PORT = process.env.PORT || 4001;
+
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 const httpServer = http.createServer(app);
-const io = new Server(httpServer, { cors: { origin: '*' } });
+const io = new Server(httpServer, { cors: { origin: "*" } });
 
-app.use(express.static('src/ui'));
+app.use(express.static("src/ui"));
 
 let buttonState = false;
 
-io.on('connection', socket => {
-    console.log('New Connection');
+io.on("connection", (socket) => {
+  console.log("New Connection");
 
-    io.to(socket.id).emit('buttonState', buttonState);
+  io.to(socket.id).emit("buttonState", buttonState);
 
-    socket.on('disconnect', () => {
-        console.log('Disconnected');
-    });
+  socket.on("disconnect", () => {
+    console.log("Disconnected");
+  });
 
-    socket.on('buttonState', value => {
-        console.log('buttonState:', value);
-        buttonState = value;
-        socket.broadcast.emit('buttonState', value);
-    });
+  socket.on("buttonState", (value) => {
+    console.log("buttonState:", value);
+    buttonState = value;
+    socket.broadcast.emit("buttonState", value);
+  });
 });
 
-httpServer.listen(PORT, () => {
-    console.log('Running on : ', httpServer.address());
+httpServer.listen(0, () => {
+  const port = httpServer.address().port;
+  
+  console.log(`Server is running on port ${port}`);
 });
